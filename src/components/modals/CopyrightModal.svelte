0c343e8314669f62d6845fe28147390f2fc73557
neil <neil.rahmouni@gmail.com>
2022-12-02 20:45:16 +0100

    Fixed #16

--- src/components/modals/CopyrightModal.svelte
+++ src/components/modals/CopyrightModal.svelte
@@ -26,18 +26,22 @@
 			<img
 				style="height:28px!important;margin-left:3px;vertical-align:text-bottom;"
 				src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
+				alt="Copyright"
 			/>
 			<img
 				style="height:28px!important;margin-left:3px;vertical-align:text-bottom;"
 				src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+				alt="Attribution"
 			/>
 			<img
 				style="height:28px!important;margin-left:3px;vertical-align:text-bottom;"
 				src="https://mirrors.creativecommons.org/presskit/icons/nc.svg"
+				alt="Non commercial"
 			/>
 			<img
 				style="height:28px!important;margin-left:3px;vertical-align:text-bottom;"
 				src="https://mirrors.creativecommons.org/presskit/icons/nd.svg"
+				alt="No derivatives"
 			/>
 		</div>
 	</a>

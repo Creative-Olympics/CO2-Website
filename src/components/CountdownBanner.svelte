b226539df144ba6da5d7b776065fc779d81ab215
neil <neil.rahmouni@gmail.com>
2022-12-04 00:07:01 +0100

    Fixed #9

--- src/components/CountdownBanner.svelte
+++ src/components/CountdownBanner.svelte
@@ -18,28 +18,28 @@
 
 		<div class="flex flex-col relative">
 			<div class="absolute w-full h-full hero-content text-center text-neutral-content flex-col">
-				<span class="uppercase text-sm font-semibold font-sans mb-2 mt-4">Event starts in</span>
-				<div class="grid grid-flow-col gap-8 text-center auto-cols-max font-bold mb-4">
+				<span class="uppercase text-sm font-semibold font-sans mb-1 sm:mb-2 mt-4">Event starts in</span>
+				<div class="grid grid-flow-col gap-6 sm:gap-8 text-center auto-cols-max font-bold mb-2 sm:mb-4">
 					<div class="flex flex-col">
-						<span class="countdown font-mono text-7xl">
+						<span class="countdown font-mono text-5xl sm:text-7xl">
 							<span style="--value:{(seconds / 86400) >> 0};" />
 						</span>
 						days
 					</div>
 					<div class="flex flex-col">
-						<span class="countdown font-mono text-7xl">
+						<span class="countdown font-mono text-5xl sm:text-7xl">
 							<span style="--value:{((seconds % 86400) / 3600) >> 0};" />
 						</span>
 						hours
 					</div>
 					<div class="flex flex-col">
-						<span class="countdown font-mono text-7xl">
+						<span class="countdown font-mono text-5xl sm:text-7xl">
 							<span style="--value:{((seconds % 3600) / 60) >> 0};" />
 						</span>
 						min
 					</div>
 					<div class="flex flex-col">
-						<span class="countdown font-mono text-7xl">
+						<span class="countdown font-mono text-5xl sm:text-7xl">
 							<span style="--value:{seconds % 60};" />
 						</span>
 						sec

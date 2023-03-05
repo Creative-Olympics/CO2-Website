02df8f99fe091a791b32ba76c4a8e208c9e54e1b
neil <neil.rahmouni@gmail.com>
2023-03-05 13:38:12 +0100

    Trying iOS detection

--- website/src/components/Banner.svelte
+++ website/src/components/Banner.svelte
@@ -35,7 +35,16 @@
 	});
 
 	let watchTrailer = () => {
-		toasts.warning("trailer")
+		toasts.warning('trailer');
+	};
+
+	if (
+		['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(
+			navigator.platform
+		) ||
+		(navigator.userAgent.includes('Mac') && 'ontouchend' in document)
+	) {
+		toasts.success("hello Z")
 	}
 </script>
 

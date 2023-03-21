908a772d82d76adf4d3bb1bc35706884eface880
neil <neil.rahmouni@gmail.com>
2023-03-21 11:21:51 +0100

    Fixed MediaQuery types

--- website/src/components/MediaQuery.svelte
+++ website/src/components/MediaQuery.svelte
@@ -1,10 +1,13 @@
 <script>
     import { onMount } from "svelte";
 
+    /** @type {string} */
     export let query;
-
+    /** @type {MediaQueryList} */
     let mql;
+    /** @type {((this: MediaQueryList, ev: MediaQueryListEvent) => any) | null} */
     let mqlListener;
+    
     let wasMounted = false;
     let matches = false;
 
@@ -22,9 +25,12 @@
         }
     }
 
+    /**
+	 * @param {string} query
+	 */
     function addNewListener(query) {
         mql = window.matchMedia(query);
-        mqlListener = v => matches = v.matches;
+        mqlListener = (/** @type {{ matches: boolean; }} */ v) => matches = v.matches;
         mql.addListener(mqlListener);
         matches = mql.matches;
     }

ded005b462b885af6be5cfd8d899c99eb5fcd51b
TretinV3 <tretinv3@laposte.net>
2023-03-21 15:34:03 +0100

    starting rebase credits

---
+++ website/src/lib/credit.js
@@ -0,0 +1,31 @@
+
+
+export const credits = [
+    {
+        name: 'Neïl Rahmouni',
+        image: 'banner/neil_rahmouni.jpg',
+        fonctions: ['Full stack developer', 'UI/UX Designer'],
+        contact: {
+            matrix: 'http://neil.rahmouni.dev/matrix',
+            twitter: 'http://neil.rahmouni.dev/twitter',
+            instagram: 'http://neil.rahmouni.dev/instagram',
+            gitlab: 'http://neil.rahmouni.dev/gitlab',
+        },
+        link: {
+            type: 'website',
+            url: '#soon',
+            name: 'Portfolio',
+        }
+    },
+    {
+        name: 'Chadrixy',
+        image: 'banner/chadrixy.png',
+        fonctions: ['Graphic designer & Artist', '3D Designer'],
+        contact: { twitter: 'http://url.creative-olympics.org/chadrixy_twitter' },
+        link: {
+            type: 'artstation',
+            url: 'http://url.creative-olympics.org/chadrixy_artstation',
+            name: 'ArtStation',
+        }
+    }
+]

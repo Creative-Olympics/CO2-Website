b96502f21b08dc56da836c3842b86bf62e3f42db
neil <neil.rahmouni@gmail.com>
2023-03-23 19:35:16 +0100

    Finished converting animations

--- website/src/routes/(landing)/+page.svelte
+++ website/src/routes/(landing)/+page.svelte
@@ -1,1 +1,55 @@
-<h1 class="h-10">Main</h1>
+<script>
+	import { goto } from '$app/navigation';
+	import { fly, fade } from 'svelte/transition';
+
+	import { scrollInstance } from '$lib/scroll';
+	import { loaderReady } from '$lib/loader';
+
+	export let loaded = true;
+
+	let watchTrailer = () => {
+		$scrollInstance?.scrollTo('top', {
+			callback: function () {
+				//goto('trailer');
+			}
+		});
+        goto('trailer');
+	};
+</script>
+
+<div class="flex w-full h-full flex-col items-center justify-center">
+	<div data-rahneiln3scroll data-rahneiln3scroll-speed="2">
+		{#if loaded && $loaderReady}
+			<img
+				in:fly={{ delay: 1000, y: -450, duration: 900 }}
+				out:fade={{ duration: 750 }}
+				src="banner/animated_logo.gif"
+				alt="CO logo"
+				class="w-80"
+			/>
+		{/if}
+	</div>
+
+	<div class="flex flew-row gap-4">
+		<div data-rahneiln3scroll data-rahneiln3scroll-speed="2" data-rahneiln3scroll-delay="0.1">
+			{#if loaded && $loaderReady}
+				<a class="btn" in:fade={{ delay: 1800 }} out:fade={{ duration: 1000 }} href="about">
+					Learn about us
+				</a>
+			{/if}
+		</div>
+		<div data-rahneiln3scroll data-rahneiln3scroll-speed="2" data-rahneiln3scroll-delay="0.07">
+			{#if loaded && $loaderReady}
+				<button
+					class="btn btn-accent text-white border-transparent hover:border-transparent bg-gradient-to-br from-green-500 via-teal-500 to-blue-500 bg-size-200 bg-pos-10 hover:bg-pos-90"
+					style="transition-property: background-position; transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); transition-duration: 400ms;"
+					in:fade={{ delay: 2050 }}
+					out:fade={{ duration: 1000 }}
+					on:click={watchTrailer}
+				>
+					Watch Trailer
+				</button>
+			{/if}
+		</div>
+	</div>
+</div>
--- website/src/routes/(landing)/trailer/+page.svelte
+++ website/src/routes/(landing)/trailer/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Trailer</h1>
+<script>
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	import { loaderReady } from '$lib/loader';
+
+	export let loaded = true;
+
+	onMount(() => {
+		mounted = true;
+	});
+</script>
+
+	<div
+		class="absolute w-full bg-black"
+		style="height: calc(100vh + 120px); z-index: 1000"
+		in:fade={{ duration: 1000, delay: 1000 }}
+        out:fade={{ duration: 1000 }}
+	/>

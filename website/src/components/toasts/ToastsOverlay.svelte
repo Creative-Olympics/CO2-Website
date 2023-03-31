98369df732423dd5c5eeeab925969bda40d15c46
neil <neil.rahmouni@gmail.com>
2023-03-31 21:05:01 +0200

    Fixed #101, #102 and #178

--- website/src/components/toasts/ToastsOverlay.svelte
+++ website/src/components/toasts/ToastsOverlay.svelte
@@ -3,10 +3,10 @@
 	import SendFeedbackToastButton from './SendFeedbackToastButton.svelte';
 </script>
 
-<div class="toast" style="z-index: 1000;">
+<div class="toast items-end" style="z-index: 1000;">
 	{#each $toasts as toast (toast.id)}
 		<div
-			class="alert {toast.type === 'alert-feedbackError' ? 'alert-error' : toast.type} shadow-lg"
+			class="alert {toast.type === 'alert-feedbackError' ? 'alert-error' : toast.type} shadow-lg w-fit"
 		>
 			<div>
 				{#if toast.type == 'alert-info'}
@@ -51,7 +51,7 @@
 							d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
 						/>
 					</svg>
-				{:else if toast.type == 'alert-error'}
+				{:else if toast.type == 'alert-error' || toast.type == 'alert-feedbackError'}
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						class="stroke-current flex-shrink-0 h-6 w-6"

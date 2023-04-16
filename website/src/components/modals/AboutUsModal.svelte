7e8b68f4dbcc1b9b56d8db8c20142104941aa343
neil <neil.rahmouni@gmail.com>
2023-04-16 16:06:37 +0200

    Fixed aboutUs images

--- website/src/components/modals/AboutUsModal.svelte
+++ website/src/components/modals/AboutUsModal.svelte
@@ -1,9 +1,15 @@
 <script>
+	import Img from "@zerodevx/svelte-img"
+
 	import { toasts } from "$lib/toasts"
 	import { rc_neilRahmouni, rc_chadrixy } from "$lib/firebase"
+
 	import SocialLinks from "$cmp/SocialLinks.svelte"
 	import SendFeedbackIconButton from "$cmp/SendFeedbackIconButton.svelte"
 	import Icon from "$cmp/Icon.svelte"
+
+	import pfp_neilRahmouni from "$lib/assets/pfp/neil_rahmouni.jpg?run"
+	import pfp_chadrixy from "$lib/assets/pfp/chadrixy.png?run&lqip=0"
 </script>
 
 <div class="absolute top-2 right-2">
@@ -16,8 +22,13 @@
 <div class="grid grid-cols-3 sm:grid-cols-7 gap-1 w-full mt-6">
 	<div class="flex flex-col items-center col-span-3">
 		<div class="avatar mb-4">
-			<div class="w-20 mask mask-squircle">
-				<img src={$rc_neilRahmouni.pfp || "banner/neil_rahmouni.jpg"} alt="Neïl Rahmouni" />
+			<div class="mask mask-squircle w-20 h-20">
+				<Img
+					src={$rc_neilRahmouni.pfp || pfp_neilRahmouni}
+					alt="Neïl Rahmouni"
+					width={80}
+					height={80}
+				/>
 			</div>
 		</div>
 		<span class="text-lg font-semibold">Neïl Rahmouni</span>
@@ -47,8 +58,13 @@
 	<div class="divider w-full sm:divider-horizontal col-span-3 sm:col-span-1 justify-self-center" />
 	<div class="flex flex-col items-center col-span-3">
 		<div class="avatar mb-4">
-			<div class="w-20 mask mask-squircle">
-				<img src={$rc_chadrixy.pfp || "banner/chadrixy.png"} alt="Chadrixy" />
+			<div class="mask mask-squircle w-20 h-20">
+				<Img
+					src={$rc_chadrixy.pfp || pfp_chadrixy}
+					alt="Chadrixy"
+					width={80}
+					height={80}
+				/>
 			</div>
 		</div>
 		<span class="text-lg font-semibold">Chadrixy</span>

06a61b1b6af1be329bc464ffb116914b9fb08326
neil <neil.rahmouni@gmail.com>
2023-04-22 18:00:49 +0200

    Fixed #149

--- website/src/components/login/AppleLoginButton.svelte
+++ website/src/components/login/AppleLoginButton.svelte
@@ -1,6 +1,7 @@
 <script>
 	/** @type {any} */
-	export let onClick;
+	export let onClick
+	export let disabled = false
 </script>
 
-<button class="btn" on:click={onClick}>Log in with Apple</button>
+<button class="btn" on:click={onClick} {disabled}>Log in with Apple</button>

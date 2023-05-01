61db77223ccb27437b3e55ce3111be3f53226680
neil <neil.rahmouni@gmail.com>
2023-05-01 17:00:33 +0200

    Added userTags

---
+++ website/src/components/UserTags.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { userTags } from "$lib/userTags"
+
+	export let className = ""
+	export let style = ""
+	export { className as class }
+	export let size = "md"
+
+	/** @type {Array<string>} */
+	export let tags = []
+</script>
+
+<div class="flex gap-1 {className}" {style}>
+	{#each userTags as userTag}
+		{#if tags.includes(userTag.id)}
+			<span
+				class="badge badge-{size}"
+				style="background-color: {userTag.color}; border-color: {userTag.color}"
+			>
+				{userTag.displayName}
+			</span>
+		{/if}
+	{/each}
+</div>

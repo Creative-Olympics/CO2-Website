247f6f708e490e75e6b93f730896563a752862e5
neil <neil.rahmouni@gmail.com>
2023-05-29 22:39:53 +0200

    Demo for loquareeous

--- website/src/routes/(landing)/+layout.svelte
+++ website/src/routes/(landing)/+layout.svelte
@@ -1,50 +1,31 @@
 <script>
-	import { fade } from "svelte/transition"
-	import { onMount } from "svelte"
-	import Img from "@zerodevx/svelte-img"
-
-	import { rc_eventTimestamp } from "$lib/firebase"
-	import { logs } from "$lib/logs"
-	import { finishedLoading, vidLoaded } from "$lib/loader"
-
-	import DonationsBanner from "$cmp/DonationsBanner.svelte"
-	import EventDesc from "$cmp/EventDesc.svelte"
-	import CountdownBanner from "$cmp/CountdownBanner.svelte"
-	import Footer from "$cmp/footer/Footer.svelte"
-	import MediaQuery from "$cmp/MediaQuery.svelte"
-
-	import banner_final from "$lib/assets/banner/final.jpg?run"
-	import banner_final_m from "$lib/assets/banner/m_final.jpg?run"
-	import isIos from "is-ios"
-
-	let mountedRn = false
-	let vidEnded = false
-
-	logs.add({ msg: "Banner mounted" }, "info")
-
-	onMount(() => {
-		mountedRn = true
-	})
+	var innerHeight;
 </script>
 
 <svelte:head>
 	<title>Creative Olympics</title>
 </svelte:head>
 
-<div
-	class="relative"
-	style="height: calc(100vh + 7rem); background-image: linear-gradient(to top right in oklab, #f00 0%, #000 0% 20%, #f00 0% 40%, #000 0% 60%, #f00 0% 80%, #000 0% 100%);"
-	data-rahneiln3scroll-section
-/>
+<div data-rahneiln3scroll-section>
+	<div
+		class="relative"
+		style="height: calc(100vh + 7rem); background-image: linear-gradient(to top right in oklab, #f00 0%, #000 0% 20%, #f00 0% 40%, #000 0% 60%, #f00 0% 80%, #000 0% 100%);"
+	/>
 
-<div class="relative" data-rahneiln3scroll-section>
-	<div class="absolute bg-blue-600 h-28 w-screen opacity-75 -top-28 left-0" />
-	<div class="bg-green-500 h-96" />
-	<div class="absolute bg-blue-600 h-28 w-screen opacity-75 -bottom-28 left-0" />
+	<div
+		class="absolute opacity-75 z-10"
+		data-rahneiln3scroll
+		data-rahneiln3scroll-speed="8"
+		data-rahneiln3scroll-position="top"
+		data-rahneiln3scroll-offset="-120"
+	>
+		<div class="bg-blue-600 h-28 w-screen" />
+		<div class="bg-green-500 w-screen" style="height: 50rem" bind:clientHeight={innerHeight} />
+		<div class="bg-blue-600 h-28 w-screen" />
+	</div>
+
+	<div
+		class="relative"
+		style="height: calc(100vh + 7rem); background-image: linear-gradient(to top right in oklab, #ff0 0%, #000 0% 20%, #ff0 0% 40%, #000 0% 60%, #ff0 0% 80%, #000 0% 100%);"
+	/>
 </div>
-
-<div
-	class="relative"
-	style="height: calc(100vh + 7rem); background-image: linear-gradient(to top right in oklab, #ff0 0%, #000 0% 20%, #ff0 0% 40%, #000 0% 60%, #ff0 0% 80%, #000 0% 100%);"
-	data-rahneiln3scroll-section
-/>

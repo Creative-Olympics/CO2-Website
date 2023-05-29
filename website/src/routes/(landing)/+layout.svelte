26f81f583110fd53bab83ac3691314b7ee1bcb95
neil <neil.rahmouni@gmail.com>
2023-05-29 23:48:04 +0200

    Fixed center section

--- website/src/routes/(landing)/+layout.svelte
+++ website/src/routes/(landing)/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	var innerHeight;
+	var innerHeight
 </script>
 
 <svelte:head>
@@ -12,16 +12,19 @@
 		style="height: calc(100vh + 7rem); background-image: linear-gradient(to top right in oklab, #f00 0%, #000 0% 20%, #f00 0% 40%, #000 0% 60%, #f00 0% 80%, #000 0% 100%);"
 	/>
 
-	<div
-		class="absolute opacity-75 z-10"
-		data-rahneiln3scroll
-		data-rahneiln3scroll-speed="8"
-		data-rahneiln3scroll-position="top"
-		data-rahneiln3scroll-offset="-120"
-	>
-		<div class="bg-blue-600 h-28 w-screen" />
-		<div class="bg-green-500 w-screen" style="height: 50rem" bind:clientHeight={innerHeight} />
-		<div class="bg-blue-600 h-28 w-screen" />
+	<div class="relative">
+		<div
+			class="absolute opacity-75 z-10 left-0"
+			data-rahneiln3scroll
+			data-rahneiln3scroll-speed="8"
+			data-rahneiln3scroll-position="top"
+			data-rahneiln3scroll-offset="-112"
+		>
+			<div class="bg-blue-600 h-28 w-screen" />
+			<div class="bg-green-500 w-screen" style="height: 50rem" bind:clientHeight={innerHeight} />
+			<div class="bg-blue-600 h-28 w-screen" />
+		</div>
+		<div style="height: {innerHeight/4}px"/>
 	</div>
 
 	<div

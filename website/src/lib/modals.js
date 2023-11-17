97f658960ad78cbc2cbd836b8b1334770a8da71c
Neïl Rahmouni <neil.rahmouni@gmail.com>
2023-11-17 18:39:01 +0100

    Improved modals

--- website/src/lib/modals.js
+++ website/src/lib/modals.js
@@ -1,57 +1,38 @@
 import { writable, derived, get } from "svelte/store"
 
 function createModalStore() {
-    /** @type {import("svelte/store").Writable<{content: any, props: any}|null>} */
-    const _modal = writable(null)
+	/** @type {import("svelte/store").Writable<{content: any, props: any, queryParamsToClear: any}|null>} */
+	const _modal = writable(null)
 
-    const modal2 = derived(_modal, ($_modal, set) => {
-        set($_modal)
-    })
-    const { subscribe } = modal2
+	const modal2 = derived(_modal, ($_modal, set) => {
+		set($_modal)
+	})
+	const { subscribe } = modal2
 
-    /**
-     * @param {any} content
-     * @param {any} props
-     */
-    function set(content, props) {
-        _modal.set({ content: content, props: props })
-    }
+	/**
+	 * @param {any} content
+	 * @param {any} props
+     * @param {any} queryParamsToClear
+	 */
+	function set(content, props, queryParamsToClear) {
+		_modal.set({ content: content, props: props, queryParamsToClear })
+	}
 
-    return {
-        set,
-        subscribe,
-        close: () => set(null, {}),
-        open: (/** @type any */ content, props = {}) => {
-            if (get(_modal) == null || get(_modal)?.content == null) {
-                set(content, props)
-            } else {
-                set(null, {})
-                setTimeout(() => set(content, props), 275)
-            }
-        }
-    }
+	return {
+		set,
+		subscribe,
+		close: () => {
+			set(null, {}, {});
+		},
+		open: (/** @type any */ content, props = {}, queryParamsToClear = "") => {
+			if (get(_modal) == null || get(_modal)?.content == null) {
+				set(content, props, queryParamsToClear);
+			} else {
+				close();
+				setTimeout(() => set(content, props, queryParamsToClear), 275)
+			}
+		}
+	}
 }
-
-// // export const modal = createModalStore()
-
-// function createModalStore() {
-// 	/** @type {import("svelte/store").Writable<{content: any, props: any}|null>} */
-// 	const {update, subscribe} = writable(null)
-
-// 	return {
-// 		subscribe,
-// 		init: (/** @type HTMLDialogElement|null */ e) => _modalEl.set(e),
-// 		close: () => console.log("TODO"),
-// 		open: (/** @type any */ content, /** @type any */ props = {}) => {
-// 			if (get(modal) == null || get(modal).content == null) {
-// 				set(this,content, props)
-// 				update(_ => {return content, props});
-// 			} else {
-// 				set(null, {})
-// 				setTimeout(() => set(content, props), 275)
-// 			}
-// 		}
-// 	}
-// }
 
 export const modal = createModalStore()

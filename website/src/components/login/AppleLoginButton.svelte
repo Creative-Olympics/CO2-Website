820165f2213ca17716c0d367ca2ed463a5b1589f
Laizo <aureliencoppee@gmail.com>
2023-12-12 11:20:53 +0100

    Update Apple login button logo based on color scheme

--- website/src/components/login/AppleLoginButton.svelte
+++ website/src/components/login/AppleLoginButton.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <LoginButton
-	logo="banner/apple_logo_dark.svg"
+	logo={window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+		? "banner/apple_logo_light.svg"
+		: "banner/apple_logo_dark.svg"}
 	text="Sign in with Apple"
 	addClass="apple font-apple"
 	ariaLabel="Sign in with Apple"
